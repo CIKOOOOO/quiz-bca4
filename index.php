65abd7e9263b64e9785701b9b33ed460bcbd043f
Hector <32627138+lewdxample@users.noreply.github.com>
2019-04-05 11:33:03 +0700

    icon

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="shrotcut icon" type="image/x-icon" href="res/favicon.ico" />
 	<title>Quiz</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
