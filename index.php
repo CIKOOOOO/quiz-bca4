39920ec2c8c47cae489e5d935b196912656f68f7
Hector <32627138+lewdxample@users.noreply.github.com>
2019-06-17 14:46:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>. -->
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="/css/style.css">
 	<script src="https://www.gstatic.com/firebasejs/5.9.1/firebase.js"></script>
 		<script>
 		  // Initialize Firebase
@@ -56,7 +56,7 @@
 	<!-- screen 2 -->
 	<div id="bodycontainer2">
 		<div class="container2">
-			<!-- <progress value="0" max="25" id="progressBar"></progress> -->
+			<progress value="0" max="25" id="progressBar"></progress>
 			<div class="box">
 				<!-- <img src="/res/bca.png" width="100px" height="100px" alt="qimg"> -->
 				<div class="question">Sudah berapa kali aplikasi Go-Jek didownload di Play Store?</div> 
